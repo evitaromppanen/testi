cabe458694a447ec3c154a69d22b0d4c33f0ed96
Evita <evita.romppanen@gmail.com>
2026-02-09 12:47:57 +0200

    Add unit test for Laskin plus method

---
+++ LaskinTesti.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class LaskinTest {
+
+    @Test
+    void testPlus() {
+        assertEquals(2, Laskin.plus(1, 1));
+    }
+}
